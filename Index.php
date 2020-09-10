d2e4f7eb791de39d9d10a0184d0d5afe148225ae
unknown <leonardoavancio@gmail.com>
2020-09-09 21:17:24 -0300

    Classe Index

---
+++ Index.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Leonardo PC
+ * Date: 09/09/2020
+ * Time: 21:13
+ */
+class Index {
+    public $init;
+}
+
+$myClass = new Index();
+echo $myClass->init = "Hello World Class";
